647f0793853532e2d0978d8f488839ca997cc9c0
jibaek <jibaek@192-168-0-118.maas>
2022-08-05 12:08:02 +0900

    test3

--- index.js
+++ index.js
@@ -1,56 +1,56 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+// const fs = require('node:fs');
+// const path = require('node:path');
+// const { Client, Collection, GatewayIntentBits } = require('discord.js');
+// const { token } = require('./config.json');
 
-const client = new Client({
-	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.MessageContent],
-});
+// const client = new Client({
+// 	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.MessageContent],
+// });
 
-client.commands = new Collection();
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-
-
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	// Set a new item in the Collection
-	// With the key as the command name and the value as the exported module
-	client.commands.set(command.data.name, command);
-}
-
-
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
-for (const file of eventFiles) {
-	const event = require(`./events/${file}`);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args, client));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args, client));
-	}
-}
+// client.commands = new Collection();
+// const commandsPath = path.join(__dirname, 'commands');
+// const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 
 
-
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-});
+// for (const file of commandFiles) {
+// 	const filePath = path.join(commandsPath, file);
+// 	const command = require(filePath);
+// 	// Set a new item in the Collection
+// 	// With the key as the command name and the value as the exported module
+// 	client.commands.set(command.data.name, command);
+// }
 
 
+// const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+// for (const file of eventFiles) {
+// 	const event = require(`./events/${file}`);
+// 	if (event.once) {
+// 		client.once(event.name, (...args) => event.execute(...args, client));
+// 	} else {
+// 		client.on(event.name, (...args) => event.execute(...args, client));
+// 	}
+// }
 
-// Login to Discord with your client's token
-client.login(token);
+
+
+
+// client.on('interactionCreate', async interaction => {
+// 	if (!interaction.isCommand()) return;
+
+// 	const command = client.commands.get(interaction.commandName);
+
+// 	if (!command) return;
+
+// 	try {
+// 		await command.execute(interaction);
+// 	} catch (error) {
+// 		console.error(error);
+// 		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+// 	}
+// });
+
+
+
+// // Login to Discord with your client's token
+// client.login(token);
