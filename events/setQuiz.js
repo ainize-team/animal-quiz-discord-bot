594137e609d614e2211b88c4a8bf42fdcc29212d
jibaek <jibaek@jibaekui-MacBookPro.local>
2022-08-09 20:12:18 +0900

    move channelId to config.json

--- events/setQuiz.js
+++ events/setQuiz.js
@@ -1,22 +1,22 @@
 import cron from 'node-cron';
-const quizChannelId = '1001379949514592348'; // 디스코드 앱에서 채널 우클릭 후 링크 복사하기를 통해 알아낼 수 있음
-const styleQuizChannelId = '1004673582963630152';
+import config from '../config.json' assert { type: 'json' };
+
+const quizChannelId = config['quizChannelId']; // 디스코드 앱에서 채널 우클릭 후 링크 복사하기를 통해 알아낼 수 있음
 let quizHandleCounter = 0;
 const quizInterval = 1;
 const totalInterval = quizInterval * 2;
+
 
 const name = 'ready';
 const once = true;
 const execute = function (client) {
   cron.schedule('* * * * *', () => {
     quizHandleCounter = (quizHandleCounter += 1) % totalInterval;
-    let channel;
+    const channel = client.channels.cache.get(quizChannelId);
     let mode;
     if (quizHandleCounter == 0) {
-      channel = client.channels.cache.get(quizChannelId);
-      mode = 'normal';
+      mode = 'drawing';
     } else if (quizHandleCounter == quizInterval) {
-      channel = client.channels.cache.get(styleQuizChannelId);
       mode = 'style';
     }
     console.log(mode + ' quiz scheduler test');
