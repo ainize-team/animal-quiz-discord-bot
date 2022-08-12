51bd0b2d89228fb22dc6180b0bc2a33a7cb3aad5
jibaek <jibaek@jibaekui-MacBookPro.local>
2022-08-12 18:19:09 +0900

    remove not using function

--- commands/ping.js
+++ commands/ping.js
@@ -4,6 +4,6 @@ const data = new SlashCommandBuilder()
   .setName('ping')
   .setDescription('Replies with Pong!');
 const execute = async function (interaction) {
-  await interaction.reply('Pongg!');
+  await interaction.reply('Pong!');
 };
 export { data, execute };
