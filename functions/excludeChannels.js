d480aff854bc5e940245b460b5f35e4c03e2bd8b
jibaek <jibaek@jibaekui-MacBookPro.local>
2022-08-16 14:21:16 +0900

    != to !==

--- functions/excludeChannels.js
+++ functions/excludeChannels.js
@@ -3,7 +3,7 @@ const quizChannelId = config['quizChannelId'];
 //const quizChannelId = process.env.CHANNEL_ID // 실제 배포시에 사용할 코드
 
 function isChannelExcluded(channelId) {
-  return channelId != quizChannelId;
+  return channelId !== quizChannelId;
 }
 
 export { isChannelExcluded };
