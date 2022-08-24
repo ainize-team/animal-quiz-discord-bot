85aa22ce893e3e05981d61e1817dbefe07a34d6b
jibaek <jibaek@192-168-0-118.maas>
2022-08-24 10:01:13 +0900

    add scheduler quiz

--- src/functions/getQuiz.js
+++ src/functions/getQuiz.js
@@ -1,6 +1,21 @@
-import { getKeyByValue } from './utils.js';
+import { getKeyByValue, getRandomInt } from './utils.js';
+import { getImagination } from './getText.js';
 import quizEmojiDict from '../../data/emojis.json' assert { type: 'json' };
+import quizStyleDict from '../../data/styles.json' assert { type: 'json' };
 const animalDict = quizEmojiDict['animalDict'];
+const styleList = quizStyleDict['styles'];
+
+function getAnimalSetAsList() {
+  let animalList = [];
+  for (let x in animalDict) {
+    animalList.push(animalDict[x]);
+  }
+  animalList = new Set(animalList);
+  animalList = [...animalList];
+  return animalList;
+}
+
+const animalList = getAnimalSetAsList();
 
 function getQuizDict(text, animal) {
   const description = text;
@@ -15,4 +30,29 @@ function getQuizDict(text, animal) {
   return quizDict;
 }
 
-export { getQuizDict };
+async function getRandomQuiz(mode) {
+  let description;
+  const animal = animalList[getRandomInt(animalList.length)];
+  const imagine = await getImagination(`${animal} shaped`);
+  description = `${animal} shaped ${imagine}`;
+  if (mode == 'style') {
+    description =
+      description +
+      ', in ' +
+      styleList[getRandomInt(styleList.length)] +
+      ' style';
+  } else if (mode == 'drawing') {
+    description = 'A painting of ' + description;
+  }
+  const quiz = description.toLowerCase().replaceAll(animal, 'X');
+  const emoji = getKeyByValue(animalDict, animal); // 이렇게 한 이유 : 한 동물에 대한 이모지가 여러개 존재하는 경우가 있기 때문
+  const quizDict = {
+    description: description,
+    quiz: quiz,
+    animal: animal,
+    emoji: emoji,
+  };
+  return quizDict;
+}
+
+export { getQuizDict, getRandomQuiz };
--- src/functions/getText.js
+++ src/functions/getText.js
@@ -1,9 +1,19 @@
-import { description, talkDescription } from '../../data/prompts.js';
+import {
+  description,
+  talkDescription,
+  designerPromptExampleList,
+} from '../../data/prompts.js';
+import { getRandomInt } from './utils.js';
 import axios from 'axios';
 import config from '../config.json' assert { type: 'json' };
 const apikey = config['apikey'];
 const OrganizationID = config['OrganizationID'];
 const url = 'https://api.openai.com/v1/completions';
+const headers = {
+  'content-type': 'application/json',
+  Authorization: 'Bearer ' + apikey,
+  'OpenAI-Organization': OrganizationID,
+};
 
 function getText(response) {
   const responseData = response['data'];
@@ -38,11 +48,6 @@ function getText(response) {
 
 async function getTalk(text) {
   const prompt = `${description} ${talkDescription}'\nme: ${text}'\nMark:`;
-  const headers = {
-    'content-type': 'application/json',
-    Authorization: 'Bearer ' + apikey,
-    'OpenAI-Organization': OrganizationID,
-  };
   try {
     const response = await axios.post(
       url,
@@ -76,4 +81,33 @@ async function getTalk(text) {
   }
 }
 
-export { getTalk };
+async function getImagination(animal) {
+  const prompt =
+    description + '\n' + designerPromptExampleList[getRandomInt(6)] + animal;
+  try {
+    const response = await axios.post(
+      url,
+      {
+        model: 'text-curie-001',
+        prompt: prompt,
+        temperature: 1,
+        max_tokens: 30,
+        top_p: 1,
+        frequency_penalty: 2.0,
+        presence_penalty: 1.0,
+        stop: ['12.', '.'],
+      },
+      {
+        headers: headers,
+      },
+    );
+
+    const imagination = getText(response);
+    return imagination;
+  } catch (e) {
+    console.log(e);
+    return 'sorry, something is wrong with the server';
+  }
+}
+
+export { getImagination, getTalk };
